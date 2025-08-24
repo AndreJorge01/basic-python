be1eba53df7c0addf9e54506c4ad5b113d6e9a1f
AndreJorge01 <andrejorge386@gmail.com>
2025-08-24 20:11:52 +0100

    variaveis&funcoesBuilt

---
+++ funcoesBuilt/built.py
@@ -0,0 +1,40 @@
+# Funcoes Built
+
+# abs() -> retorna o valor absoluto de um numero
+numeroNegativo = -10
+valorNumeroNegativo = abs(numeroNegativo)
+print(valorNumeroNegativo)
+
+# min() -> retorna o valor minimo de uma sequencia numerica
+minDaSequencia = (6, 7, 8, 9, 10)
+valorMin = min(minDaSequencia)
+print(valorMin)
+
+# max() -> retorna o valor maximo de uma sequencia numerica
+
+maxDaSequencia = (6, 7, 8, 9, 10)
+valorMax = max(maxDaSequencia)
+print(valorMax)
+
+# sum() -> retorna a soma de uma sequencia numerica
+
+sumDaSequencia = ({6, 7, 8, 9, 10})
+valorSum = sum(sumDaSequencia)
+print(valorSum)
+
+# pow() -> exponenciacao
+x = 3
+y = 3
+exponencicao = pow(x, y)
+print(exponencicao)
+
+# print() -> mostra o valor de saida de uma variavel
+nome = 'andre'
+print(nome)
+
+# input() -> le um valor digitado no teclado do usuario
+seuNome = input('qual é o seu nome?')
+print(seuNome)
+
+# help() -> mostra as funcionalidades de uma funcao
+help(max)
---
+++ variaveis/variaveis.py
@@ -0,0 +1,8 @@
+# semelhante ao javaScript
+
+nome = 'andre'
+sobreNome = 'jorge'
+idade = '22'
+pais = 'Brazil'
+
+print(nome + sobreNome + idade + pais)
