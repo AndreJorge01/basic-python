b9fc5faa3ae75448c7dc3378d015d246d87845a7
AndreJorge01 <andrejorge386@gmail.com>
2025-08-24 19:23:16 +0100

    first commit

---
+++ operadores/operadores.py
@@ -0,0 +1,69 @@
+# soma
+x = 5
+y = 6
+soma = x + y
+print (soma)
+print(4+5)
+print(6.5+4)
+print(6+4)
+
+#  subtracao
+
+subtracao = x - y
+print (subtracao)
+print(10-5)
+
+# multiplicacao
+
+multiplicacao = x * y
+print(multiplicacao)
+print (10*10)
+
+# divisao
+
+divisao = x / y
+print(divisao)
+print(100/4)
+t = 20
+v = 4
+divisaoInteira = t // v # // retorna somente o valor inteiro
+print(divisaoInteira)
+
+i = 10
+l = 3
+
+divisaoResto = i % l # % na divisao retorna so o resto por exemplo 10/3 sobra 1 porque 3*3 e 9 falta 1 para 10
+print(divisaoResto)
+
+# Operadores logicos
+# boleanos true e false
+# e logicos maior, menor, maior ou igual, menor ou igual, igualdade e diferenca
+
+# maior que >
+
+maior = x > y 
+print (maior)
+
+# menor que < 
+
+menor = x < y
+print (menor)
+
+# maior que >=
+print(10>10)
+print(10>=10)
+
+# menor que <=
+print(10<10)
+print(10<=10)
+
+# igualdade ==
+igualdade = x == y
+print(igualdade)
+print(5 == 5)
+
+# diferenca !=
+
+diferenca = x != y 
+print(diferenca)
+print (5 != 5)
