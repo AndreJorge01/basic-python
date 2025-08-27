cacad606e69d27d7b00b629b86588ba3b61e17fd
AndreJorge01 <andrejorge386@gmail.com>
2025-08-27 23:04:21 +0100

    repeticaoWhile

---
+++ repeticaoWhile/while.py
@@ -0,0 +1,20 @@
+# lacos while
+
+# estrutura
+
+# while condicao:
+# comados 1
+# comandos 2
+
+numero = 0
+while numero <= 10:
+    print(numero)
+    numero += 1  # ou numero = numero + 1
+print('the loop end')
+
+numeroMax = eval(input("Enter a number"))
+numero = 0
+while numero <= numeroMax:
+    print(numero)
+    numero += 1
+print('the loop end')
